d537ad81dfe6de0db3e42285fa3a58dc353c0618
Lloydied2day <202411848@gordoncollege.edu.ph>
2025-04-04 23:28:33 +0800

    d

--- Traffic Lights/Lightson.js
+++
@@ -1,9 +0,0 @@
-function turnOn() {
-    document.getElementById('bulbImage').style.display = 'block'; 
-    document.getElementById('offImage').style.display = 'none';  
-}
-
-function turnOff() {
-    document.getElementById('bulbImage').style.display = 'none';  
-    document.getElementById('offImage').style.display = 'block'; 
-}
